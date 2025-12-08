bb464533146dfaf4e9c35fd902a9f572d398c6e7
Leandre02 <113716341+Leandre02@users.noreply.github.com>
2025-12-08 13:58:14 -0500

    Gebug helmet server.ts

--- src/server.ts
+++ src/server.ts
@@ -84,8 +84,6 @@ app.use(express.static(staticDir));
 
 // rend disponible la documentation de l'interface logicielle
 app.get('/api-docs/', async (req, res) => {
-    res.set('Content-Security-Policy', 'script-src blob:');
-    res.set('Content-Security-Policy', 'worker-src blob:');
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
